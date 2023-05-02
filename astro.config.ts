536621dfefd48a71f41ee8fbd87e5836b897f8d8
AmeMeida <ame.pistache@gmail.com>
2023-05-01 21:57:06 -0300

    agr vai o github pages

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://amemeida.github.io/accesscityastro",
-  base: "/accesscityastro/",
+  site: "https://amemeida.github.io/AccessCityAstro",
+  base: "/AccessCityAstro",
 });
